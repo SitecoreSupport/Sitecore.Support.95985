509690b2f9b2316707c0c68bf85d3888f87883e8
DK\hly <hly@sitecore.net>
2018-10-14 13:34:24 -0400

    solve the issue that Engagement Plan state does not change in v8.1.1

---
+++ src/Sitecore.Support.95985/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.95985")]
+[assembly: AssemblyProduct("Sitecore.Support.95985")]
+[assembly: ComVisible(false)]
